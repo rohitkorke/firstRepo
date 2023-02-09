aa7ba46c52c19625aecd9ce2e6710d0ec232f149
rohitkorke <94745636+rohitkorke@users.noreply.github.com>
2023-02-09 13:40:26 +0530

    Update jenkinsfilesloader.groovy

--- jenkinsfilesloader.groovy
+++ jenkinsfilesloader.groovy
@@ -1,1 +1,15 @@
 
+pipeline {
+    agent any
+    options {
+        // Timeout counter starts AFTER agent is allocated
+        timeout(time: 1, unit: 'SECONDS')
+    }
+    stages {
+        stage('Build') {
+            steps {
+                echo 'Hello World'
+            }
+        }
+    }
+}
